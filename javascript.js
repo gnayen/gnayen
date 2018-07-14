b6c38b3f48b8232076d2920ac24dc3e7d4b9d69a
Gna <ooooxoxooooo@gmail.com>
2018-07-14 23:12:56 +0800

    portfolio 0714-4

--- javascript.js
+++ javascript.js
@@ -5,12 +5,22 @@ $(document).ready(function(){
 	});
 
 	// #scroll_navbar
+	// $(window).scroll(function(){
+	// 	console.log($(window).scrollTop());
+	// 	if($(window).scrollTop() < 405){
+	// 		$('#scroll_navbar').fadeOut(400);
+	// 	}
+	// 	if($(window).scrollTop() > 406){
+	// 		$('#scroll_navbar').fadeIn(400);
+	// 	}
+	// });
+
 	$(window).scroll(function(){
 		console.log($(window).scrollTop());
-		if($(window).scrollTop() < 405){
+		if($(window).scrollTop() < $('#banner').height()){
 			$('#scroll_navbar').fadeOut(400);
 		}
-		if($(window).scrollTop() > 406){
+		if($(window).scrollTop() > $('#banner').height()){
 			$('#scroll_navbar').fadeIn(400);
 		}
 	});
@@ -36,17 +46,30 @@ $(document).ready(function(){
 	});
 
 
+	// $(".intro_button").on('click',function(){
+	// 	$("html, body").animate({scrollTop: 768}, "slow");
+	// });
+
 	$(".intro_button").on('click',function(){
-		$("html, body").animate({scrollTop: 768}, "slow");
+		$("html, body").animate({scrollTop:$("#intro").offset().top}, "slow");
 	});
+
+	// $(".work_button").on('click',function(){
+	// 	$("html, body").animate({scrollTop: 1432}, "slow");
+	// });
 
 	$(".work_button").on('click',function(){
-		$("html, body").animate({scrollTop: 1432}, "slow");
+		$("html, body").animate({scrollTop:$("#work").offset().top - $('#scroll_navbar').height() +'px'}, "slow");
 	});
 
+	// $(".contact_button").on('click',function(){
+	// 	$("html, body").animate({scrollTop: 1885}, "slow");
+	// });
+
 	$(".contact_button").on('click',function(){
-		$("html, body").animate({scrollTop: 1885}, "slow");
+		$("html, body").animate({scrollTop:$("#contact").offset().top}, "slow");
 	});
+
 
 	$(".go_up").on('click',function(){
 		$("html, body").animate({scrollTop: 0}, "slow");
